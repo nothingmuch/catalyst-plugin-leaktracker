4f638344311e3fe176b0d88f45ad5f564be4dd34
Yuval Kogman <nothingmuch@woobling.org>
2007-09-13 02:45:00 +0000

    Refactor a bunch of methods from Controller::LeakTracker

--- lib/Catalyst/Plugin/LeakTracker.pm
+++ lib/Catalyst/Plugin/LeakTracker.pm
@@ -195,6 +195,96 @@ sub create_devel_events_filter_chain {
     );
 }
 
+sub get_all_request_ids {
+    my $c = shift;
+    map { my ( $type, %req ) = @$_; $req{request_id} } $c->get_all_request_begin_events;
+}
+
+sub get_all_request_begin_events {
+    my $c = shift;
+    $c->devel_events_log->grep("request_begin");
+}
+
+sub get_request_events {
+    my ( $c, $request_id ) = @_;
+    $c->devel_events_log->limit( from => { request_id => $request_id }, to => "request_end" );
+}
+
+sub get_all_request_events_hash {
+    my $c = shift;
+
+    my %events_by_id;
+
+    foreach my $request_id ( $c->get_all_request_ids ) {
+        $events_by_id{$request_id} = [ $c->get_request_events($request_id) ];
+    }
+
+    return \%events_by_id;
+}
+
+sub get_all_request_events {
+    my $c = shift;
+
+    map { { id => $_, events => [ $c->get_request_events($_) ] } } $c->get_all_request_ids;
+}
+
+sub get_event_by_id {
+    my ( $c, $event_id ) = @_;
+
+    if ( my $event = ( $c->devel_events_log->grep({ id => $event_id }) )[0] ) {
+        return @$event;
+    } else {
+        return;
+    }
+}
+
+sub generate_stack_for_event {
+    my ( $c, $request_id, $event_id ) = @_;
+
+    my @events = $c->devel_events_log->limit( from => { request_id => $request_id }, to => { id => $event_id } );
+
+    my @stack;
+    foreach my $event ( @events ) {
+        my ( $type, %data ) = @$event;
+
+        if ( $type eq 'enter_action' ) {
+            push @stack, \%data;
+        } elsif ( $type eq 'leave_action' ) {
+            pop @stack;
+        }
+    }
+
+    return @stack;
+}
+
+sub get_object_tracker_by_id {
+    my ( $c, $request_id ) = @_;
+    $c->object_tracker_hash->{$request_id};
+}
+
+sub get_object_entry_by_id {
+    my ( $c, $request_id, $id ) = @_;
+
+    if ( my $tracker = $c->get_object_tracker_by_id($request_id) ) {
+        my $live_objects = $tracker->live_objects;
+
+        foreach my $obj ( values %$live_objects ) {
+            return $obj if $obj->{id} == $id;
+        }
+    }
+
+    return;
+}
+
+sub get_object_by_event_id {
+    my ( $c, $request_id, $id ) = @_;
+
+    if ( my $entry = $c->get_object_entry_by_id( $request_id, $id ) ) {
+        return $entry->{object};
+    } else {
+        return;
+    }
+}
 
 __PACKAGE__;
 
